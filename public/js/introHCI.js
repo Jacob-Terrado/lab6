f544c9d2623db9615ca139ea50eb487eb5628530
Jacob Terrado <jsterrad@ucsd.edu>
2016-02-09 08:27:24 +0000

    Finishe the lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,17 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	console.log('User clicked on project ' + idNumber);
+
+	$.get('/project/' + idNumber, function(result) {
+		console.log("result = " + result);
+		var projectHTML = '<a href="#" class="thumbnail">' + 
+			'<img src="' + result['image'] + '"alt="" class="img detailsImage"></a>' + 
+			'<p>' + result['summary'] + '</p>' +
+			'<p><small>' + result['date'] + '</small></p>';
+
+		$('#project' + idNumber).find('.thumbnail').find('.details').html(projectHTML);
+	})
 }
 
 /*
@@ -35,4 +45,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get('/palette', function(result) {
+		var colors = result['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
